77747b8d734f447cc119ef02d9f5c8def373fddb
damaashok <ashokchowdary9515@gmail.com>
2024-03-20 09:13:39 +0530

    Update BeanExample.java

--- src/main/java/com/example/demo/BeanExample.java
+++ src/main/java/com/example/demo/BeanExample.java
@@ -11,7 +11,7 @@ public class BeanExample {
 	public List<StudentInfoEntity> m1() {
 			List<StudentInfoEntity> obj1=new ArrayList<StudentInfoEntity>();
 			StudentInfoEntity obj2=new StudentInfoEntity();
-			obj2.setName("Ashokhchowdary12");
+			obj2.setName("Ashok12");
 			obj2.setEmailid("ashok@gmail.com");
 			obj1.add(obj2);
 		return obj1;
